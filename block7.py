2d2b0d93c32981ea76ad171a83f686aa2b94230b
Duuuda <60673731+Duuuda@users.noreply.github.com>
2020-07-09 16:28:01 +0300

    Add_other_plots

--- block7.py
+++ block7.py
@@ -9,7 +9,17 @@
 #
 # import modules -------------------------------------------------------------------------------------------------------
 from matplotlib import pyplot as plt
+from matplotlib.ticker import Formatter
 # ----------------------------------------------------------------------------------------------------------------------
+
+
+# Formatter class
+class CustomFormatter(Formatter):
+    def __init__(self, confirmed):
+        self.confirmed = confirmed
+
+    def __call__(self, y, pos=0):
+        return y
 
 
 # slice data -----------------------------------------------------------------------------------------------------------
@@ -23,7 +33,8 @@ def slice_by_countries(func):
                                                                    'Deaths',
                                                                    'Recovered',
                                                                    'Recovered/Confirmed %',
-                                                                   'Deaths/Confirmed %']], country
+                                                                   'Deaths/Confirmed %',
+                                                                   'ObservationDate']], country
     return wrapper
 
 
@@ -36,6 +47,12 @@ def get_data():
 
 # plot graphs ----------------------------------------------------------------------------------------------------------
 def plot_graph(data, country):
+    data = data[['Price',
+                 'Confirmed',
+                 'Deaths',
+                 'Recovered',
+                 'Recovered/Confirmed %',
+                 'Deaths/Confirmed %']]
     fig = plt.figure(figsize=(15, 10))
     plt.matshow(data.corr(), fignum=fig.number, cmap='inferno')
     ticks = ['BTC Price'] + data.columns.tolist()[1:-2] + ['Recovered÷\nConfirmed %', 'Deaths÷\nConfirmed %']
@@ -45,6 +62,39 @@ def plot_graph(data, country):
     cb.ax.tick_params(labelsize=20)
     plt.title(f'{country} correlation matrix', fontsize=25, pad=40)
     plt.show()
+    # На данных графиках наблюдается некоторая зависимость количества подтвержденных случаев от цены биткоина
+    # Для более подробного рассмотрения завистимости построим 2 дополнительных графика
+
+
+def plot_graph_2(all_data):
+    for data_and_country in all_data:
+        data, country_name = data_and_country
+        formatter = CustomFormatter(data['Confirmed'])
+        fig = plt.figure(figsize=(20, 10))
+        ax1 = fig.add_subplot(111)
+        ax1.yaxis.set_major_formatter(formatter)
+        ax1.plot(data['ObservationDate'],
+                 data['Confirmed'],
+                 '-g',
+                 label="Confirmed",
+                 lw=2)
+        ax1.set_ylabel('Количество заболевших чел.', color='green')
+        ax1.legend(loc='upper left')
+        ax1.grid(True)
+        ax2 = ax1.twinx()
+        ax2.yaxis.set_major_formatter(formatter)
+        ax2.plot(data['ObservationDate'],
+                 data['Price'],
+                 '-r',
+                 label="BTC_Price",
+                 lw=2)
+        ax2.set_ylabel('Цена Биткоина $', color='red')
+        ax2.legend(loc='upper right')
+        ax2.grid(True)
+        plt.title(country_name)
+        plt.show()
+        # После построения данных графиков, действительно видна зависимость двух (на первый взгляд независимых) параметров
+        # Из этого можно сделать вывод, что мы имеем дело с ложной корреляцией
 # ----------------------------------------------------------------------------------------------------------------------
 
 
@@ -52,6 +102,7 @@ def plot_graph(data, country):
 def main():
     for country_data in get_data():
         plot_graph(*country_data)
+    plot_graph_2(get_data())
 # ----------------------------------------------------------------------------------------------------------------------
 
 
